d693cc3081e4aed54f01ad830cd60cea3be91a5b
AnozieChibuike <chibuikeanozie0@gmail.com>
2025-04-12 00:20:45 +0100

    Refactor verification message and remove in-app browser detection

--- src/pages/auth/Verify.jsx
+++ src/pages/auth/Verify.jsx
@@ -3,28 +3,6 @@ import { useNavigate } from "react-router-dom";
 import REACT_APP_SERVER_URL from "../../constant";
 
 const Verify = () => {
-const [inApp, setInApp] = useState(false);
-
-function checkBrowser () {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    // Detect Gmail and other common in-app browsers
-    const isInAppBrowser = /(FBAN|FBAV|Instagram|Line|Snapchat|WhatsApp|Twitter|Gmail|Android.*WebView|iPhone.*Safari\/|CriOS)/i.test(userAgent);
-    setInApp(isInAppBrowser)
-    if (isInAppBrowser) {
-        // Notify the user
-        setMessage("It looks like you're using an in-app browser. For the best experience, please open this link in your main browser.");
-
-        // Optionally provide a button to redirect
-        const redirectButton = document.createElement('button');
-        redirectButton.innerText = "Open in Browser";
-        redirectButton.style = "position: fixed; bottom: 20px; right: 20px; padding: 10px; background-color: #007bff; color: white; border: none; border-radius: 5px; cursor: pointer;";
-        redirectButton.onclick = () => {
-            window.location.href = window.location.href; 
-        };
-        document.body.appendChild(redirectButton);
-      }
-}
 
   const [message, setMessage] = useState("Verifying...");
   const navigate = useNavigate();
@@ -47,8 +25,8 @@ function checkBrowser () {
         if (data.email) {
           // Store email in session
           localStorage.setItem("email", data.email);
-          setMessage("Verification successful! Redirecting...");
-          if (!is_admin) setTimeout(() => navigate("/voter-dashboard"), 2000);
+          setMessage(<span>Verification successful! Redirecting...<br><span>If page doesn't automatically redirect, click this <a href="/admin">link</a></span></br></span>);
+          if (!is_admin) setTimeout(() => navigate("/admin"), 2000);
           else setTimeout(() => navigate("/admin"), 2000);
         } else {
           setMessage("Verification failed.");
